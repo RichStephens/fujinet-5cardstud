b16ff23e1219c2658e35c291352ccadd8fbc1bb3
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2026-03-19 18:50:11 -0500

    [platform-specific] sync over.

---
+++ src/platform-specific/input.h
@@ -0,0 +1,22 @@
+/*
+  Platform specific input.
+*/
+#ifndef INPUT_H
+#define INPUT_H
+
+// Include platform specific defines before the input include
+#include "../c64/vars.h"
+#include "../apple2/vars.h"
+#include "../adam/vars.h"
+#include "../coco/vars.h"
+#include "../msdos/vars.h"
+#include "../plus4/vars.h"
+
+// Platform specific implementations
+unsigned char readJoystick();
+
+#ifdef USE_PLATFORM_SPECIFIC_INPUT
+int getPlatformKey(void);
+#endif
+
+#endif /* INPUT_H */
---
+++ src/platform-specific/network.h
@@ -0,0 +1,9 @@
+/*
+  Network functionality
+*/
+#ifndef NETWORK_H
+#define NETWORK_H
+
+uint8_t getResponse(char *url, unsigned char *buffer, uint16_t max_length);
+
+#endif /* NETWORK_H */
